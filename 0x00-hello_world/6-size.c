1a25ee6033db0ea168a2a62468ea34c8281a9ca4
Fagbenro iseoluwa <iseoluwaf30@gmail.com>
2023-08-12 20:12:17 -0700

    size-c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main entry
+ *
+ * return: Always 0(success)
+ */
+int main(void)
+{
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of long int: %d byte(s)\n", sizeof(long imt));
+	printf("Size of long long int: %d byte(s)\n", sizeof(lomg long int));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
