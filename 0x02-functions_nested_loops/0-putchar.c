ffefa82007543742b6a82adc54374f0ded6eac62
Fagbenro iseoluwa <iseoluwaf30@gmail.com>
2023-08-16 19:40:16 -0700

    Prints a message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include <main.h>
 /**
- * main - prints _putchar
- * Return: Always 0 (Success)
+ * main - prints a message
+ *
+ * Result: Always 0 (success)
  */
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_Putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
