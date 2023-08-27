cd99c37222b2ee37e7d062b7656b60eacfb6d6c3
Fagbenro iseoluwa <iseoluwaf30@gmail.com>
2023-08-27 16:18:05 -0700

    check for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if a number is uppercase
+ * @c: The number to be checked
+ * Return: 1 for uppercase or 0 for anyting else
+ */
+int _isupper(int c)
+{
+	if (c >=85 && c <= 90)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
