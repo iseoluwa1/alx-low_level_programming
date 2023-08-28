2a0ba0342f6bc1c944c12a5f2fd43716e78bb9ff
Fagbenro iseoluwa <iseoluwaf30@gmail.com>
2023-08-27 20:10:20 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line according to parameter
+ * @n: the number of line to draw
+ * Return: empty
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
