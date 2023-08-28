32c9369caa77e08835c20ecb289c230670a86c94
Fagbenro iseoluwa <iseoluwaf30@gmail.com>
2023-08-27 17:55:03 -0700

    multipliy digit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: The first number to multiply
+ * @b: The second number to multiply with a
+ * return: Always res
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
